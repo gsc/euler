56f7e1c2dae21f0609357a39d84591ee42142f47
gsc <guillelmo@gmail.com>
2015-06-08 23:40:47 +0200

    Fixed p3: format and generalized compute_primer_factors

--- euler/p3.py
+++ euler/p3.py
@@ -1,38 +1,39 @@
-# Project Euler, problem 3: Largest Prime factor (http://projecteuler.net/problem=3). 
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
 
-# The prime factors of 13195 are 5, 7, 13 and 29.
-# What is the largest prime factor of the number 600851475143 ?
+"""
+Project Euler, problem 3: Largest Prime factor
+http://projecteuler.net/problem=3
 
-num = 600851475143
+The prime factors of 13195 are 5, 7, 13 and 29.
+What is the largest prime factor of the number 600851475143 ?
+"""
 
-#Initialization: 2 as the first prime, the rest will be computed. 
-primes = [2]
+def compute_prime_factors(num, floor=1):
+    """
+    Computes the prime factor of a number by brute force.
+    Two small optimizations: only checks the odd factors (even are never prime,
+    except for 2), and floor is the lowest possible prime (so it doesn't start
+    counting on 3 on each call.
+    """
 
-# Compute the primes in the range [3,2000], skipping the even numbers (which are always multiples of 2). 
-for i in range(3,2000,2):
-    j = 0
-    while j < len(primes) and i % primes[j] !=0:
-        j += 1
-    if(j >= len(primes)):
-        primes.append(i)
+    if num % 2 == 0:
+        return [2] + compute_prime_factors(num / 2, 3)
 
-# This function will return the factors of num.
-# Note: this version works for 600851475143, because it's a product of [71, 839, 1471, 6857] (i.e all of its factors are primes in the range [3,2000], except
-# for 6857, which is a prime as well. The prime calculation needs to be improved to handle numers with more than one prime factor bigger than 2000.
-def getFactors(num):
-    if(num == 1):
-        return [1]
-    
-    for i in primes:        
-        if num % i == 0:
-            l = [i]
-            return l + getFactors(num / i)
-    print('not in primes: '+str(num))
-    return [num]
+    if floor % 2 == 0:
+        # All the possible prime factors are odd, so the search will start in
+        # the next one
+        floor += 1
 
+    while floor < num:
+        floor += 2
+        if num % floor == 0:
+            return [floor] + compute_prime_factors(num / floor, floor)
 
+    return [1]
 
-       
-            
-            
-    
+if __name__ == "__main__":
+    NUM = 600851475143
+    FACTORS = compute_prime_factors(NUM, 1)
+
+    print "The largest prime factor of %d is %d " % (NUM, max(FACTORS))
