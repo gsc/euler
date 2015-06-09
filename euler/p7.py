7c047a60dc1d5465bd8dc7077fc85cfd12ee0a41
gsc <guillelmo@gmail.com>
2015-06-09 22:55:19 +0200

    Migrated p7 to python

---
+++ euler/p7.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Project Euler, problem 7: 10001st prime (http://projecteuler.net/problem=7).
+
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that
+the 6th prime is 13.
+
+
+What is the 10001st prime number?
+"""
+
+def is_prime(num):
+    """
+    Determines whether a number is a prime
+    """
+    for i in range(2, int(num**0.5)+1):
+        if num % i == 0:
+            return False
+
+    return True
+
+def main():
+
+    primes = [1, 2, 3]
+    last_prime = 3
+
+    while len(primes) < 10002:
+        last_prime += 2
+        if is_prime(last_prime):
+            primes.append(last_prime)
+
+    print "The 10.001st prime is %d " % last_prime
+
+if __name__ == "__main__":
+    main()
