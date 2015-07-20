823bfa905b3342f40df2436f600cecde3dbcd331
gsc <guillelmo@gmail.com>
2015-07-21 00:19:28 +0200

    Fixed format, and excluding 1900 from the counting

--- euler/p19.py
+++ euler/p19.py
@@ -22,29 +22,42 @@ How many Sundays fell on the first of the month during the twentieth century (1
 Jan 1901 to 31 Dec 2000)?
 """
 
-months = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31,
+MONTHS = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31,
           11: 30, 12: 31}
 
 def is_leap_year(year):
+    """
+    A leap year occurs on any year evenly divisible by 4, but not on a century
+    unless it is divisible by 400.
+    """
     if year % 100 == 0:
-        return (year % 400 == 0)
+        return year % 400 == 0
     else:
-        return (year % 4 == 0)
+        return year % 4 == 0
 
 def get_month_days(month, year):
+    """
+    Returns the number of days in a month (from dictionary, except for february
+    on leap years).
+    """
     if is_leap_year(year) and month == 2:
         return 29
     else:
-        return months[month]
+        return MONTHS[month]
 
-def get_mondays():
+def main():
+    """
+    Given that we know that 1/1/1900 is a monday, we can compute all the mondays
+    in the 20th century. The number of sundays on the first day of the month
+    would the same as the number of mondays on the second day of the month.
+    """
     day = 1
     month = 1
     year = 1900
 
     days_of_month = get_month_days(month, year)
 
-    count = 0
+    mondays = 0
 
     while year < 2001:
         day += 7
@@ -58,14 +71,10 @@ def get_mondays():
 
             days_of_month = get_month_days(month, year)
 
-        if day == 1:
-            count += 1
+        if day == 2 and year > 1900:
+            mondays += 1
 
-    return count
-
-def main():
-    res = get_mondays()
-    print "There was %d mondays between 1900 and 2000" % res
+    print "There were %d mondays between 1900 and 2000" % mondays
 
 if __name__ == "__main__":
     main()
